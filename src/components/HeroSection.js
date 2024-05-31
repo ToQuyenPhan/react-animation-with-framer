0b99a8456a34bc812d531381f786697ab551fe13
ToQuyenPhan <duonglh024@gmail.com>
2024-05-31 20:17:40 +0700

    update animations

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import styled from 'styled-components';
 import { motion } from 'framer-motion';
+import PlanetOne from '../images/planet.svg';
+import PlanetTwo from '../images/planet-2.svg';
+import PlanetThree from '../images/planet-3.svg';
+import PlanetFour from '../images/planet-4.svg';
 
 const Section = styled.section`
     height: 100vh;
@@ -41,31 +45,79 @@ const ColumnLeft = styled.div`
     }
 `;
 
-const Button = styled.div`
+const Button = styled(motion.button)`
     padding: 1rem 3rem;
     font-size: 1rem;
     border: 2px solid white;
     border-radius: 4px;
     outline: none;
     background: transparent;
+    color: white;
     cursor: pointer;
 `;
 
-const ColumnRight = styled.div`
+const Image = styled(motion.img)`
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    max-width: 250px;
+    max-height: 250px;
+`;
 
+const ColumnRight = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    padding: 2rem;
+    position: relative;
+
+    ${Image}:nth-child(1){
+        top: 10px;
+        left: 10px;
+    }
+
+    ${Image}:nth-child(2){
+        top: 170px;
+        right: 10px;
+    }
+
+    ${Image}:nth-child(3){
+        top: 350px;
+        left: 50px;
+    }
+
+    ${Image}:nth-child(4){
+        bottom: 100px;
+        right: 75px;
+    }
 `;
 
 const HeroSection = () => {
+    const fadeLeft = {
+        hidden: { opacity: 0, x: -100 },
+        visible: { opacity: 1, x: 0 }
+    }
+
     return (
         <Section>
             <Container>
                 <ColumnLeft>
-                    <h1>Welcome to Space</h1>
-                    <p>Journey to the unknown</p>
-                    <Button>Get Started</Button>
+                    <motion.h1 initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ duration: 1 }}>
+                        Welcome to Space
+                    </motion.h1>
+                    <motion.p variants={fadeLeft} initial='hidden' animate='visible' transition={{ duration: 1 }}>Journey to the unknown</motion.p>
+                    <Button whileHover={{ scale: 1.05 }} whileTap={{
+                        scale: 0.95, backgroundColor: '#67F6E7', border: 'none',
+                        color: '#000'
+                    }} initial={{ opacity: 0 }} animate={{ opacity: 1, transition: { duration: 1.5 } }}>
+                        Get Started
+                    </Button>
                 </ColumnLeft>
                 <ColumnRight>
-                filler
+                    <Image src={PlanetOne} alt="planet" whileTap={{ scale: 0.9}}/>
+                    <Image src={PlanetTwo} alt="planet" />
+                    <Image src={PlanetThree} alt="planet" />
+                    <Image src={PlanetFour} alt="planet" />
                 </ColumnRight>
             </Container>
         </Section>
