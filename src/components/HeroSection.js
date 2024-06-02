b14a35c77604941d38a9bcea639c876fe2f6cae8
ToQuyenPhan <duonglh024@gmail.com>
2024-06-02 13:58:21 +0700

    done

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -18,7 +18,7 @@ const Container = styled.div`
     display: grid;
     grid-template-columns: 1fr 1fr;
     height: 100vh;
-    padding: 3rem calc((100vw - 1300px) / 2);
+    padding: 0rem calc((100vw - 1300px) / 2);
 
     @media screen and (max-width: 760px) {
         grid-template-columns: 1fr;
@@ -114,10 +114,18 @@ const HeroSection = () => {
                     </Button>
                 </ColumnLeft>
                 <ColumnRight>
-                    <Image src={PlanetOne} alt="planet" whileTap={{ scale: 0.9}}/>
-                    <Image src={PlanetTwo} alt="planet" />
-                    <Image src={PlanetThree} alt="planet" />
-                    <Image src={PlanetFour} alt="planet" />
+                    <Image src={PlanetOne} alt="planet" whileTap={{ scale: 0.9}} drag={true} 
+                        dragConstraints={{ left: 0, right: 250, top: 0, bottom: 50}} initial={{ opacity: 0, y: -100}}
+                        animate={{ opacity: 1, y: 10, transition: { duration: 1}}} />
+                    <Image src={PlanetTwo} alt="planet" whileTap={{ scale: 0.6}} drag={true} 
+                        dragConstraints={{ left: 50, right: 0, top: 0, bottom: 50}} initial={{ opacity: 0, x: 100}}
+                        animate={{ opacity: 1, x: 10, transition: { duration: 1}}} />
+                    <Image src={PlanetThree} alt="planet" whileTap={{ scale: 0.8}} drag={true} 
+                        dragConstraints={{ left: 0, right: 250, top: 0, bottom: 50}} initial={{ opacity: 0, x: -100}}
+                        animate={{ opacity: 1, x: 10, transition: { duration: 1}}} />
+                    <Image src={PlanetFour} alt="planet" whileTap={{ scale: 0.9}} drag={true} 
+                        dragConstraints={{ left: 0, right: 0, top: 0, bottom: 0}} initial={{ opacity: 0, y: 100}}
+                        animate={{ opacity: 1, y: 10, transition: { duration: 1}}} />
                 </ColumnRight>
             </Container>
         </Section>
